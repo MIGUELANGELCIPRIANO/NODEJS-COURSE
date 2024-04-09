9da9261ba273d9c9f3c5c9396e44d0bc4bbd2d3c
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2024-04-09 14:42:49 -0300

    TEST: JavaScript in Node.js

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log('Hello world!')
+// console.log('Hello world!')
+
+// let username = 'John'
+// let age = 30
+// let hasHobbies = true
+// let points = [10, 20, 30]
+// let user = {
+// 	name: 'John',
+// 	lasname: 'Doe',
+// }
+// const PI = 3.1415
+
+// console.log(username)
+// console.log(age)
+// console.log(hasHobbies)
+// console.log(points)
+// console.log(user)
+// console.log(PI)
+
+// const age = 30
+
+// age >= 18 ? console.log('You are an adult') : console.log('You are a child')
+
+// const names = ['Joe', 'John', 'Marco']
+
+// for (let i = 0; i < names.length; i++) {
+// 	console.log(names[i])
+// }
+
+const showUserInfo = (userName, userAge) =>
+	`The username is ${userName}, the user is ${userAge} years old`
+
+console.log(showUserInfo('Joe', 30))
+console.log(showUserInfo('John', 20))
