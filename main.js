fba7b8d5bf6f636f326594cc3a33aa744ae50924
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2024-04-09 15:19:57 -0300

    TEST: CommonJS Modules

---
+++ main.js
@@ -0,0 +1,7 @@
+const web = require('./module/module')
+
+console.log(web)
+
+const { address1, address2, user } = require('./module/module')
+
+console.log(user)
---
+++ module/module.js
@@ -0,0 +1,12 @@
+const address1 = 'magicthegathering.com'
+const address2 = 'wizardsofthecoast.com'
+const user = {
+	name: 'Мигель Анхель',
+	password: 'xxxxxxxxxxxxx',
+}
+
+module.exports = {
+	address1,
+	address2,
+	user,
+}
