52a351090763a0558f9517bf5af4c25958652d1e
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2024-04-10 14:07:31 -0300

    TEST: fs (async) module

--- fs.js
+++ fs.js
@@ -1,5 +1,7 @@
 const fs = require('fs')
 
+// SYNC
+
 const first = fs.readFileSync('./data/first.txt', 'utf-8')
 const second = fs.readFileSync('./data/second.txt')
 
@@ -10,4 +12,18 @@ const text = 'My third commit '
 
 fs.writeFileSync('./data/third.txt', text, {
 	flag: 'a',
+})
+
+// ASYNC
+
+fs.readFile('./data/first.txt', (error, data) => {
+	error ? console.log(error) : console.log(data.toString())
+
+	fs.readFile('./data/second.txt', 'utf-8', (error, data) => {
+		error ? console.log(error) : console.log(data)
+
+		fs.writeFile('./data/fourth.txt', 'My fourth commit', (error, data) => {
+			error ? console.log(error) : console.log(data)
+		})
+	})
 })
