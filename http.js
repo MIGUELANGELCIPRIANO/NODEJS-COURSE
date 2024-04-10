9c71e552fbdc483991c3734174233ae448b2639b
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2024-04-10 15:04:31 -0300

    TEST: http requests

--- http.js
+++ http.js
@@ -1,10 +1,26 @@
 const http = require('http')
 
-http
-	.createServer((req, res) => {
-		res.write('Hello world!')
-		res.end()
-	})
-	.listen(3000)
+const server = http.createServer((req, res) => {
+	console.log(req.url)
+
+	if (req.url === '/') {
+		res.write('Home page')
+		return res.end()
+	}
+
+	if (req.url === '/about') {
+		res.write('About us')
+		return res.end()
+	}
+
+	res.write(
+		`<h1>Not Found</h1>
+            <p>The site does not exist</p>
+            <a href="/">Back to home page</a>`
+	)
+	res.end()
+})
+
+server.listen(3000)
 
 console.log('Server listening on port 3000')
